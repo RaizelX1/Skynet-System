11ab5c3411269d7f1a0a9c613f250e1cc87be8cf
Raizel <115201759+RaizelX1@users.noreply.github.com>
2022-10-14 21:18:00 -0700

    Create config.py

---
+++ Skynet_System/config.py
@@ -0,0 +1,12 @@
+import Skynet_System.config as Config
+
+
+  # REQUIRED
+    API_ID_KEY = "9241939"
+    API_HASH_KEY = "591ea4cb50c59f5b0880a8fd74241ae1"  
+    STRING_SESSION = "1AZWarzwBu6KeDNaoAhsh2uuVj7XMrgYmxeiWXdwI9vtsI2oDOq6vAGLVtoZ52SWQ2Ph1KAHF46YLzGA9-9j6wbhZmPuiH1dWVRyrjbMUpUgqa54gp24-CPpEdpIbov5Lr_ptU2AanD51GA1W6PSAGqNJP_7x6W_nERXjwE1wcNQS61zfjgbvOf1ipDogDuEvYOfm28ifIsCnHKPSiKPqtdrnc3BP_wl_scCArfU7o9wUWRmyzEpGJ64G8tawGtaT0D2WUyTtZWngQ-ajSGTnrfO8tLO-mw1UE-nO-sw9klC44Bya_Nw2Cid3JHRLtTBg7SOLNAN__Fb8rb0gZfocJf1ztviEiMk="
+    Skynet = "5465875229"
+    ENFORCERS = "5465875229"
+    Skynet_logs = "-1001791894910"
+    Skynet_approved_logs = "-1001791894910"
+    GBAN_MSG_LOGS = "-1001791894910"
